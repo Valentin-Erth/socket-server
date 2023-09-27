e1fe6614e2412d4d923232b22516572e6151db1a
Kozlov Valentin <kozlov0020@gmail.com>
2023-09-27 20:32:49 +0500

    feat:add headers in route

--- src/route.ts
+++ src/route.ts
@@ -3,6 +3,16 @@ import express from "express";
 const router=express.Router()
 //создали endpoint
 router.get("/", (req,res)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
     res.send("Hi, It's Websocket server!")
 })
 export default router
