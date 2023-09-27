5f9ff1ca2a6854d029de3d29dd1fe2cb9da7d5a5
Kozlov Valentin <kozlov0020@gmail.com>
2023-09-27 21:20:14 +0500

    fix

--- src/route.ts
+++ src/route.ts
@@ -4,15 +4,15 @@ const router=express.Router()
 //создали endpoint
 router.get("/", (req,res)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader(
-    //     "Access-Control-Allow-Methods",
-    //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    // );
-    //
-    // res.setHeader(
-    //     "Access-Control-Allow-Headers",
-    //     "X-Requested-With,content-type"
-    // );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
     res.send("Hi, It's Websocket server!")
 })
 export default router
